7faa0a375040e56aa51b78606a9a453fb3072696
scelios <scelios@outlook.fr>
2025-03-20 10:11:10 +0100

    pass on school pc

--- vaccine/vaccine/vaccine.py
+++ vaccine/vaccine/vaccine.py
@@ -4,6 +4,7 @@ import sys
 import time
 import requests
 import json
+import re
 
 class Vaccine:
     def __init__(self, args):
@@ -11,7 +12,6 @@ class Vaccine:
         self.output = args.o
         self.request_type = args.X
         self.input = self.extract_form_inputs(self.get_data_from_url())
-        self.payloads = self.get_payloads()
 
     def get_data_from_url(self):
         match self.request_type:
@@ -36,6 +36,28 @@ class Vaccine:
 
     def get_payloads(self):
         os.open("payloads.txt", "r")
+
+    def extract_form_inputs(self, data):
+        return re.findall(r'<input[^>]*>', data)
+    
+    def send_SQL_injection(self):
+        for root, _, files in os.walk("injection/detect/"):
+            for file in files:
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, "r") as f:
+                        payload = f.read()
+                        filename = file.split(".")[0]
+                        for input in self.input:
+                            requests.post(self.url, data={input: payload})
+                            with open(self.output, "a") as f:
+                                f.write(f"Payload: {payload} - Input: {input}\n")
+                except Exception as e:
+                    print(f"Error reading file {file_path}: {e}")
+    
+    def check_database(self):
+        
+
 def get_args():
     parser = argparse.ArgumentParser(
         description="SQL Injection tester, will test for SQL Injection vulnerabilities in a given URL"
